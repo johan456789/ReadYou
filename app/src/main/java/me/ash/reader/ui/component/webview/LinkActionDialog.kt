40e64e17bbcf5b6aaa2ff1e3e2de5fbf3593946f
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2026-05-03 01:38:10 +0800

    refactor(reading): remove link action cancel button

--- app/src/main/java/me/ash/reader/ui/component/webview/LinkActionDialog.kt
+++ app/src/main/java/me/ash/reader/ui/component/webview/LinkActionDialog.kt
@@ -20,7 +20,6 @@ import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -107,11 +106,7 @@ fun LinkActionDialog(
                 )
             }
         },
-        confirmButton = {
-            TextButton(onClick = onDismissRequest) {
-                Text(text = stringResource(R.string.cancel))
-            }
-        },
+        confirmButton = {},
     )
 }
 
