acdd60f9e46d06ae2052d328f8be2277e1b3c694
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2026-09-15 15:52:41 +0800

    chore(release): bump versionCode to 54

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "com.thyu.reader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 53
+        versionCode = 54
         versionName = "0.16.1"
 
         buildConfigField(
